ceb1529f21fd9b7e7514cc0f217e2564df880315
Marina Huber <huber.marina@gmail.com>
2019-12-16 17:25:56 +0100

    Add Storyboard protocol for separate storyboards

--- Marvel-Hero-demo/Marvel-Hero-demo/StoryboardProtocol.swift
+++ Marvel-Hero-demo/Marvel-Hero-demo/StoryboardProtocol.swift
@@ -9,21 +9,21 @@
 import UIKit
 
 protocol StoryboardProtocol: AnyObject {
-	static func initialize(from storyboard: UIStoryboard) -> Self
+    static func instantiate(from storyboard: UIStoryboard) -> Self
 }
 
 extension StoryboardProtocol where Self: UIViewController {
-	static func initialize(from storyboard: UIStoryboard) -> Self {
-		let nameSpaceClassName: String = NSStringFromClass(self)
+    static func instantiate(from storyboard: UIStoryboard) -> Self {
+        let nameSpaceClassName: String = NSStringFromClass(self)
 
-		guard let className: String = nameSpaceClassName.components(separatedBy: ".").last else {
-			fatalError("Cannot find class name from: " + nameSpaceClassName)
-		}
+        guard let className: String = nameSpaceClassName.components(separatedBy: ".").last else {
+            fatalError("Cannot find class name from: " + nameSpaceClassName)
+        }
 
-		guard let viewController: Self = storyboard.instantiateViewController(withIdentifier: className) as? Self else {
-			fatalError("Cannot find UIViewController with identifier: " + className)
-		}
+        guard let viewController: Self = storyboard.instantiateViewController(withIdentifier: className) as? Self else {
+            fatalError("Cannot find UIViewController with identifier: " + className)
+        }
 
-		return viewController
-	}
+        return viewController
+    }
 }
